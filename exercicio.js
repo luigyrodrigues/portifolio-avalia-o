cc94b42ec023dadb4e52e550cccc088b608854fc
unknown <luigyrodrigues54@gmail.com>
2026-02-25 20:59:41 -0300

    atividade java script

---
+++ exercicio.js
@@ -0,0 +1,901 @@
+// let num1 = 2;
+// let num2 = 10;
+
+// console.log("soma: " + (num1 + num2));
+// console.log("subtração: " + (num1 - num2));
+// console.log("multiplicação: " + (num1 * num2)); 
+// console.log("divisão: " + (num1 / num2));
+
+// let base = 10;
+// let altura = 5;
+
+// console.log("area base do retângulo: " + (base * altura ))
+
+// let real = 50;
+// let dólar = 5.0;
+// let resultado  = real / dólar;
+// console.log(resultado);
+
+// let num = 10;
+// let cubo = num ** 2;
+// console.log(cubo)
+
+// let num1 = 7;
+// let num2 = 6;
+// let num3 = 5;
+// let num4 = 9;
+
+// // console.log("média é:" + ((num1+num2+num3+num4))/4)
+// let c = 32.5 
+// let f = (c * 9 / 5 )+  32 ;
+// console.log ( " A conversão de " + c + " é "  + ( f ) )
+
+// let resultado = 15 % 4 ;
+// console.log ( resultado ); 
+
+// let valor = 150
+// let desconto = 0.15;
+// let desc =  desconto  * valor; 
+// console.log (desc)
+
+// let num = 4 
+// console.log (` O antecessor de ${num} é ${num-1}`)
+// console.log (` O sucessor  de ${num} é ${num+1}`)
+
+// let peso= 70;
+// let altura = 1.60;
+// let soma = ( altura * 2 ) / peso;
+// console.log (`O IMC É ${soma}` ) 
+
+// let num1 = 10==="10" ;
+// console.log ( `A comparação estreita entre 10 e "10" é: ${num1}`)
+
+// let idade = 15
+// console.log ( `A idade é maior que 18 ? ${idade>18} `)
+// console.log ( `A idade é menor que 18 ? ${idade<18} `)
+
+// let valor = 100;
+// let desconto = 15;
+// let resultado = valor * desconto / 100;
+
+// console.log("o valor inicial era de:") + valor + "novo valor:" *(valor - resultado + "desconto é" =))
+
+// let num = 10;
+// let sucess = num + 1;
+// let ant = num - 1;
+
+// console.log(num, sucess, ant);
+
+// let peso = 80;
+// let altura = 1.60;
+// let resultado = peso / altura;
+
+// console.log("o seu imc é:" + resultado);
+
+// let numero = 10;
+// let texto = "10";
+// let comparacao = (numero === texto);
+
+// console.log(comparacao);
+
+// let idade = 39;
+// if(idade >=18)
+// console.log("a sua idade é: " + idade +" anos");
+
+// let Javascript = "Javascript";
+// let javascript = "javascript";
+
+// if(Javascript == javascript){
+//     console.log("sim, as strings são iguais")
+// }
+// else{
+//     console.log("não, as strings são diferentes")
+// }
+
+// let num = 10;
+// if(num <= 0){
+//     console.log("sim, o número é menor ou igual a 0");
+// }
+// else{
+//     console.log("não, o número é maior do que 0");
+// }
+
+// let sT = "queijo"
+// if(sT.length > 5){
+//     console.log("sim, é maior do que 5: " + sT.length)
+// }
+// else{
+//     console.log("não, é menor do  que 5: " + sT.length)
+// }
+
+// let num1 = 5 + 5;
+// let num2 =  2 * 5;
+// if(num1 == num2){
+//     console.log("sim, os resultados são iguais")
+// }
+// else{
+//     console.log("não, os resultados são diferentes")
+// }
+
+// let saldo = 60;
+// let saque = 20;
+// if(saldo <= saque){
+//     console.log("saldo disponivel:")
+// }
+// else{
+//     console.log("saldo indisponivel:")
+// }
+
+// let n1 = typeof true;
+// let n2 = typeof "true";
+// if(n2 === n1){
+//     console.log("sim, as variaveis são iguais:")
+// }
+// else{
+//     console.log("não, as variaveis são diferentes:")
+// }
+
+// let script = 35;
+// if(script >= 10 &&  script<= 50){
+//     console.log("sim, o numero está entre o intervalo:")
+// }
+// else{
+//     console.log("não, o numero não está entre o intervalo:")
+// }
+
+// let num1 = "admin";
+// let num2 = "1234";
+// if(num1 === "admin" && num2 === "1234"){
+//     console.log("login aprovado:")
+// }
+// else{
+//     console.log("falha no login:")
+// }
+
+// let idade = 40;
+// if(idade > 18 && idade < 70){
+//     console.log("este usuario pode votar:")
+// }
+// else{ 
+//     console.log("acesso negado:")
+// }
+
+// let num = -3;
+// if(num % 2 === 0 && num > 0){
+//     console.log("sim, esse numero é par e positivo")
+// }
+// else{
+//     console.log("não, esse numero não é par e nem positivo")
+// }
+
+// let hoje = "domingo";
+// if(hoje === "domingo" || hoje === "sabado"){
+//     console.log("hoje é domingo:")
+// }
+// else{
+//     console.log("hoje é sabado:")
+// }
+
+// let estachovendo = true;
+
+// console.log("está chovendo:", !estachovendo)
+
+// let media = 5;
+// let faltas = 15;
+// if(media >=7 && faltas <=10){
+//     console.log("este aluno está aprovado")
+// }
+// else{
+//     console.log("este aluno esta reprovado")
+// }
+
+// let num = 10;
+// if(num !== 0){
+//     console.log(true)
+// } 
+// else{
+//     console.log(false)
+// }
+
+// let a = 5;
+// let b = 10;
+// if(a !== b){
+//     let temp = a;
+//     a = b;
+//     b = temp;
+//     console.log("a troca de valores foi realizada")
+// }
+
+// let direita = 5;
+// let esquerda = 6;
+// let ponta = 5
+// if(direita + esquerda > ponta){
+//     console.log("a soma de direita e esquerda é maior que ponta")
+// }
+// else{
+//     console.log("a soma de direita e esquerda e menor que ponta")
+// }
+
+// let cachorro = 7;
+// let idadehumana = cachorro *7;
+// console.log("a idade do cachorro " + cachorro + " convertendo para idade do humano é " + idadehumana)
+
+
+// let valordacompra = 1000;
+// let parcelas = 5;
+// let juros = 0.02;
+
+// let valorcomjuros = valordacompra * (1 + juros * parcelas);
+// let valorcomparcelas = valorcomjuros / parcelas;
+
+// console.log("o valor com juros é igual a:" + valorcomjuros);
+// console.log("o valor com parcelas é igual a:" + valorcomparcelas);
+
+// switch case
+
+// let dia = 5;
+
+// switch(dia) {
+//     case 1:{
+//         console.log("domingo")
+//     }break;
+//         case 2:{
+//         console.log("segunda");
+//         break;
+
+//     }
+//     case 5: {
+//         console.log("quinta");
+//         break;
+//     }
+//     case 6: {
+//         console.log("sexta");
+//         break;
+//     }
+//     case 7: {
+//         console.log("sábado");
+//         break;
+//     }
+//     default:{
+//         console.log("não é um dia da semana");
+//         break;
+//     }
+// }
+
+// let mes = "janeiro";
+
+// switch (mes){
+
+//     case "janeiro": {
+//         console.log("verão");
+//         break;
+//     }
+//     case "fevereiro": {
+//         console.log("verão");
+//         break;
+//     }
+//      case "março": {
+//         console.log("verão/outono");
+//         break;
+//     }
+//      case "abril": {
+//         console.log("outono");
+//         break;
+//     }
+//     case "maio": {
+//         console.log("outono");
+//         break;
+//     }
+//      case "junho": {
+//         console.log("outono/inverno");
+//         break;
+//     }
+//     case "julho": {
+//         console.log("inverno");
+//         break;
+//     }
+//     case "agosto": {
+//         console.log("inverno");
+//         break;
+//     }
+//     case "setembro": {
+//         console.log("inverno/primavera");
+//         break;
+//     }
+//     case "outubro": {
+//         console.log("primavera");
+//         break;
+//     }
+//     case "novembro": {
+//         console.log("primavera");
+//         break;
+//     }
+//      case "dezembro": {
+//         console.log("primavera/verão");
+//         break;
+//     }
+//     default:
+//             console.log("não é um mês do ano");
+//             break;
+
+// }
+
+
+// let num = 20;
+// let num2 = 10;
+// let operação = "-";
+
+// switch (operação){
+//     case "+": {
+//         console.log(num + num2);
+//         break;
+//     }
+//     case "-": {
+//         console.log(num - num2)
+//         break;
+//     }
+//     case "*": {
+//         console.log(num * num2)
+//         break;
+//     }
+//     case "/": {
+//         console.log(num / num2)
+//         break;
+//     }
+//     default:
+//         console.log("operação inválida");
+//         break;
+// }
+//  let letra = "B";
+
+//  switch (letra) {
+//     case "A": {
+//         console.log("exelente")
+//         break;
+//     }
+//     case "B": {
+//         console.log("bom")
+//         break;
+//     }
+//     case "C": {
+//         console.log("regular")
+//         break;
+//     }
+//     case "D": {
+//         console.log("ruim")
+//         break;
+//     }    
+//     case "E": {
+//         console.log("reprovado")
+//         break;
+//     }
+//     default:
+//         console.log("nenhuma das opções")  
+//         break;
+//  }
+
+// let pedido = 3;
+
+// switch (3) {
+//     case 1: {
+//         console.log("X-burger, preço: 12,00")
+//         break;
+//     }
+//     case 2: {
+//         console.log("X-salada, preço: 10,00")
+//     }
+//     case 3: {
+//         console.log("batata frita, preço: 10,00")
+//         break;
+//     }
+//     case 1: {
+//         console.log("refrigerante, preço: 7,00")
+//         break;
+// }
+//     default:
+//         console.log("você não fez um pedido")
+//         break;
+// }
+
+// let letra = "A";
+
+// switch (letra) {
+
+//     case "A": 
+//     case "E":
+//     case "I":
+//     case "O":    
+//     case "u": {
+//         console.log("está letra é uma vogal")
+//         break;
+//     }
+//     default:
+//         console.log("consoante ou outro caractere")
+//         break;
+// }
+
+// let plano = "prata";
+
+// switch (plano){
+//     case "ouro": {
+//         console.log("Exclusividade, atendimento VIP, máximo de benefícios.")
+//         break;
+//     }
+//     case "prata": {
+//         console.log("Custo-benefício, melhores serviços.");
+//         break;
+//     }
+//     case "bronze": {
+//         console.log("Acesso básico, custo reduzido.")
+//         break;
+//     }
+//     default:
+//         console.log("nenhum dos beneficios")
+//         break;
+// }
+
+// let reais = 20.00;
+// let  moeda = "USD";
+// let conversão;
+
+// switch (moeda){
+//     case "USD": {
+//     console.log(conversão = reais * 0.19);
+//     break;
+//     }
+//     case "EUR": {
+//         console.log(conversão = reais * );
+//         break;
+//     }
+//     case "GBP": {
+//         console.log(conversão = reais / 7.0);
+//     }
+//     default:
+//         console.log("não houve nenhuma conversão")
+// //         break;
+// }
+// let reais = 20.00;
+// let  moeda = "USD";
+// let conversão;
+
+// switch (moeda){
+//     case "USD": {
+//     console.log(conversão = reais * 0.19);
+//     break;
+//     }
+//     case "EUR": {
+//         console.log(conversão = reais * 0.16);
+//         break;
+//     }
+//     case "GBP": {
+//         console.log(conversão = reais * 0.14);
+//         break;
+//     }
+//     default:
+//         console.log("não houve nenhuma conversão")
+//         break;
+// }
+
+
+// let categoria = "idoso";
+
+// switch (categoria){
+//     case "criança": {
+//         console.log("faixa etária de 0 a 12 anos");
+//         break;
+//     }
+//         case "adolescente": {
+//         console.log("faixa etária de 12 a 18 anos");
+//         break;
+//     }
+//     case "adulto": {
+//         console.log("faixa etária de 18 a 59 anos");
+//         break;
+//     }
+//     case "idoso": {
+//         console.log("apartir dos 60 anos");
+//         break;
+//     }
+//     default:
+//         console.log("faixa etária não identificada")
+//         break;
+// }
+ 
+// let veiculo = "carro";
+
+// switch(veiculo){
+//     case "carro": {
+//         console.log("este veiculo pagará 3 pedágios")
+//         break;
+//     }
+//         case "moto": {
+//         console.log("este veiculo pagará 2 pedágios")
+//         break;
+//     }
+//     case "caminhão": {
+//         console.log("este veiculo pagará 5 pedágios")
+//         break;
+//     }
+
+// }
+
+// let dia = 3;
+
+// switch(dia){
+//     case 2:
+//     case 3: 
+//     case 4:
+//     case 5:{
+//         console.log("dia útil")
+//         break;
+//     }
+//     case 6:
+//     case 7:{
+//         console.log("fim de semana")
+//         break;
+//     }
+//     default:
+//         console.log("não é nem dia útil nem fim de semana")
+//         break;
+
+// }
+
+// let num = 3;
+
+// switch(num){
+//     case 1: {
+//         console.log("aguardando")
+//         break;
+//     }
+//     case 2: {
+//         console.log("enviado")
+//         break;
+//     }
+//     case 3: {
+//         console.log("entregue")
+//         break;
+//     }
+//     default:
+//         console.log("status inválido")
+//         break;
+// }
+
+// let letra = "N";
+
+// switch(letra){
+//     case "N": {
+//         console.log("Norte")
+//         break;
+//     }
+//     case "S": {
+//         console.log("Sul")
+//         break;
+//     }
+//     case "L": {
+//         console.log("Leste")
+//         break;
+//     }
+//     case "O": {
+//         console.log("Oeste")
+//         break;
+//     }
+//     default:
+//         console.log("")
+
+// }
+
+// ATIVIDADE 14
+
+// let a = "vaca"
+
+// switch (a){
+//     case "gato": {
+//         console.log(`${a} faz : miau`)
+//         break;
+//     }
+//     case "cão": {
+//         console.log(`${a} faz : Auau`)
+//         break;
+//     }
+//     case "ovelha": {
+//         console.log(`${a} faz : beeeh`)
+//         break;
+//     }
+//     case "vaca" : {
+//         console.log(`${a} faz : Muu`)
+//         break;
+//     }
+//     default: {
+//     console.log("invalido")
+//     break;
+//     }
+// }
+
+// let i;
+// for (i = 0; i < 5; i++) {
+    
+//     if(i == 3 || i == 4) {
+//         continue;
+//     }
+//     console.log(`valor de i: ${i}`);
+// }
+//     console.log(`valor de i: ${i}`);
+
+// let op = 0;
+// while(op == 0){
+//     console.log(`hello world`);
+//     op = 2;
+// }
+// do {
+//     console.log(`hello world2`);
+// }while (op == 0);
+
+// let i;
+//  for(i = 0; i < 10; i++) {
+//     if(i == 3)
+//     continue;
+//     console.log(`permitido crianças de: ${i}`)
+//  }
+
+// let dpc = 10;
+// while(dpc > 0){
+//     console.log("Manga")
+//     dpc--;
+// }
+
+
+// let dpc = 10;
+// do{
+//    let j = 99;
+
+//    j += 235;
+//    if(j >= 300) {
+//     console.log("J é " + j);
+//    }
+//    dpc--;
+// }while(dpc > 0);
+
+// quetão 1
+
+// for(let i = 0; i < 20; ++i){
+//     console.log(i);
+// }
+// questão 2
+
+// for(let i = 10; i >= 1;--i){
+//     console.log(i);
+// }
+// questão 3
+
+// for(let i = 1; i <= 15; i++){
+//      if(i % 2 == 1)
+//      continue;
+//      console.log(i);
+// }
+// questão 4
+// for(let i = 1; i <= 10; i++){
+//      console.log(`7 x ${i} = ${7*i}`)
+// }
+// questão 5
+// let soma = 0;
+// for(let i = 1; i <= 10; i++){
+//     soma += i;
+// }
+// questão 6
+// console.log("a soma total é " + soma)
+
+// let num = "javascript";
+
+// for(let i = 0; i < num.length; i++){
+//      console.log(num [i])
+// }
+// questão 7
+
+// for (let i = 0; i <= 50; i += 5){
+//      console.log("a soma total é " + i)
+// }
+// questão 8
+
+// for (let i = 1; i <= 20; i++){
+//      if(i % 2 !== 0)
+//      continue;
+//      console.log(i);
+// }
+// questão 9
+// for(let i = 1; i <= 10; i++){
+//      console.log(`${i} "ao quadrado é " ${i ** 2}`)
+// }
+// questão 10
+// let num = "**********";
+
+// for(let i = 0; i < num.length; i++){
+//      console.log(num [i])
+// }
+
+// atividade while
+
+// let i = 0;
+// while(i < 11){
+//      console.log(i);
+//      i++;
+// }
+// questão 2 
+
+// let num = 25;
+// while(num <= 100) {
+//     console.log("esse numero e menor que o solicitado")
+//     num++;
+// }
+// questão 3
+// 
+// let cronometro = 10;
+
+// while (cronometro >= 0) {
+//     console.log(cronometro);
+
+//     if (cronometro === 0) {
+//         console.log("lançar!");
+//     }
+//     cronometro--;
+// }
+
+// questão 4
+
+// let i = 1;
+// let soma = 0;
+// let quantidade = 5;
+
+// while(i <= quantidade){
+//     let numero = i * 2;
+//     soma += numero;
+//     i++;
+// }
+// let media = soma / quantidade;
+// console.log("media" ,  media);
+
+// correção
+
+// let media = 0;
+// while (true){
+//     let num1 = 60;
+//     let num2 = 55;
+//     let num3= 1;
+//     let num4 = 11;
+//     let num5 = 7;
+    
+//     media = (num1 + num2 + num3 + num4 + num5  ) / 2;
+//     break;
+// }
+
+// console.log(media);
+// questão 5
+
+// let cont = 0;
+// while((cont * 7) < 50){
+//     ++cont;
+// }
+// console.log(cont);
+// questão 6
+
+// let num = 5;
+// while(num !== 5){
+//     num = Math.floor(Math.random()*10 + 1);
+//     console.log("o numero gerado foi " + num)
+// }
+// console.log("o numero sorteado foi " + num)
+
+// questão 7
+// let i = 1;
+// let soma = 0;
+// while(soma <= 50){
+//     soma += i;
+//     i++;
+// }
+// console.log(soma)
+// questão 8
+// let i = 1;
+
+// while(i < 1000){
+//     i *= 2;
+//     console.log(i);
+// }
+// questão 9
+
+// let altura = 1;
+// let dia = 1;
+// while(altura < 15){
+//    console.log("dia" , dia , "altura" , altura ,  "cm ")
+//    altura += 2;
+//    dia++;
+// }
+//    console.log("dia" , dia , "altura" , altura ,  "cm ")
+
+// questão 10
+
+// let i = false;
+// while (!i){
+//     console.log("olá mundo");
+//     i = true;
+// }
+
+// let dpc = 10;
+// do{
+//    let j = 99;
+
+//    j += 235;
+//    if(j >= 300) {
+//     console.log("J é " + j);
+//    }
+//    dpc--;
+// // }while(dpc > 0);
+
+// let senha = "100";
+
+// do{
+//     senha = prompt("digite a senha");
+// }while (senha !== "123");
+//    alert("acesso liberado");
+// ATVD #6
+
+// let js = "javascript"
+// for (let i = 0; i < js.length; i++) {
+//     console.log(js [i]);
+// }
+
+
+// // ATVD #7
+
+
+// for (let i = 0 ; i  < 50 ; i += 5 ){
+//     console.log(i); 
+// }
+
+// ATVD #8
+
+// let soma = 0
+// for (let i = 1 ; i <=20; i++){
+//     if (i % 2 == 1 ) continue; 
+//     soma += i;
+        
+// }
+// console.log (soma);
+
+// ATVD #9
+
+// let expo=0 
+// for (let i = 1; i <=10 ; i++){
+// expo = i ** 2 ;
+// console.log(expo);
+// }
+
+
+
+// ATVD #10
+
+
+
+// let aster = "" ;
+// for (let i  = 0 ; i < 10; i++ ) {
+//     aster += "*" ; 
+// }
+// console.log(aster)
+
+// ATVD #11
+
+// let c = 0 ;
+// while(c <10 ){
+//     console.log(c);
+//     c++;
+// } 
+
+
+// ATVD #11
+
+// let n = 10 
+// while ( n <100 ){
+//     console.log("continue")
+    
+
+// }
+    
+
